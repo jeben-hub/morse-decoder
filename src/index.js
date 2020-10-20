9170b04521c019e4b2a813d2c6227071eb844484
jeben-hub <abondaronok@gmail.com>
2020-10-20 14:12:47 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let ch_arr = [];
+    for (let i = 0; i < expr.length; i += 10){
+        ch_arr.push(expr.slice(i, i + 10));
+    }
+    let BINARY_TABLE = {}, binary_key;
+    Object.keys(MORSE_TABLE).forEach(el => {
+        binary_key = el;
+        while(binary_key != (binary_key = binary_key.replace("-", "11"))) {}
+        while(binary_key != (binary_key = binary_key.replace(".", "10"))) {}
+        if(binary_key.length != 10) 
+            BINARY_TABLE["0000000000".slice(-(10 - binary_key.length)) + binary_key] = MORSE_TABLE[el];
+        else BINARY_TABLE[binary_key] = MORSE_TABLE[el];
+    }) 
+    BINARY_TABLE["**********"] = " ";
+    let rez_arr = [];
+    ch_arr.forEach(code =>{
+        rez_arr.push(BINARY_TABLE[code]);
+    });
+    return rez_arr.join("");
 }
 
 module.exports = {
